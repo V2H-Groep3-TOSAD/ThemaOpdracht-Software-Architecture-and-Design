ffd0d1caebd3db0b743644bbd1be354e1c56e457
Remco <remcotaal@hotmail.com>
2020-08-15 12:59:48 +0200

    Hibernate test

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,12 +1,24 @@
 package org.openjfx;
 
 import java.io.IOException;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import org.domain.Operator;
+import org.services.OperatorService;
+import org.services.impl.OperatorServiceImpl;
 
 public class PrimaryController {
+
+    private OperatorService service = new OperatorServiceImpl();
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+        List<Operator> operators = service.getAllOperators();
+        for (Operator operator : operators){
+            System.out.println(operator.getName());
+        }
+
     }
 }
