f57a590a16b1f7d0865b830ee10e8c38974b39bd
Sanne <sanne.vanderlinden@student.hu.nl>
2020-08-17 15:43:15 +0200

    annotatieAdd

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -9,6 +9,7 @@ public class PrimaryController {
 
     private PostgresServiceProvider postgresServiceProvider = new PostgresServiceProvider();
 
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
 
