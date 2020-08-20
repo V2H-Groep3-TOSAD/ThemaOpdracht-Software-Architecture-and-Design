e6b7f8cfc0461fcfada80392180748330f180c86
Aron Ruizendaal <aronruizendaal@gmail.com>
2020-08-20 19:01:18 +0200

    DatabaseController update

--- src/main/java/org/openjfx/DatabaseController.java
+++ src/main/java/org/openjfx/DatabaseController.java
@@ -1,14 +1,44 @@
 package org.openjfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
-import java.awt.*;
+import javafx.scene.control.ChoiceBox;
+import org.business.controllers.DefineBusinessRuleController;
+import org.business.domain.Database;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseController {
+
+    DefineBusinessRuleController defineController = new DefineBusinessRuleController();
+    @FXML
+    ChoiceBox<String> databaseBox;
+
+
+
+    public void initialize() {
+        System.out.println(defineController.giveAllDatabases());
+
+        ArrayList<Database> lijst = (ArrayList<Database>)defineController.giveAllDatabases();
+
+        ObservableList<String> options = FXCollections.observableArrayList();
+        for(Database db : lijst){
+            System.out.println(db.getName());
+            options.add(db.getName());
+        }
+        System.out.println(options);
+        databaseBox.getItems().addAll(options);
+
+
+    }
 
     @FXML
     private void nextKnopClick() throws IOException {
         App.setRoot("table");
+
+        String selectedChoice = databaseBox.getSelectionModel().getSelectedItem();
+        System.out.println(selectedChoice);
     }
 }
--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -4,15 +4,26 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import org.services.PostgresServiceProvider;
+import org.business.controllers.DefineBusinessRuleController;
+import org.openjfx.DatabaseController;
+
+
 
 public class PrimaryController {
 
-    private PostgresServiceProvider postgresServiceProvider = new PostgresServiceProvider();
+    private DefineBusinessRuleController defineController = new DefineBusinessRuleController();
+    private DatabaseController databaseController = new DatabaseController();
 
     @FXML
     private void switchToDefine() throws IOException {
+
         App.setRoot("database");
+
+
+
+
     }
+
     @FXML
     private void switchToGenerate() throws IOException {
         App.setRoot("generate");
