1800d37e4827615f1e336ce27bcef41c68763c95
Aron Ruizendaal <aronruizendaal@gmail.com>
2020-08-20 15:50:51 +0200

    Range.fxml toegevoegd generate en define.fxml geupdate

--- src/main/java/org/openjfx/DefineController.java
+++ src/main/java/org/openjfx/DefineController.java
@@ -1,4 +1,18 @@
 package org.openjfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.io.IOException;
+
 public class DefineController {
+
+    public ComboBox businessRuleType;
+
+    @FXML
+    private void volgendeKnopClick() throws IOException {
+        if (businessRuleType.getValue().toString().equals("Range")) {
+            App.setRoot("range");
+        }
+    }
 }
