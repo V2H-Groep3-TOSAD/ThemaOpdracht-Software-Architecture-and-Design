6c7f5869fdc858a1257723fcdabde7269d891ea1
lennart-1 <lennartkoster@live.nl>
2020-08-28 19:18:53 +0200

    fix

--- src/main/java/org/openjfx/DatabaseController.java
+++ src/main/java/org/openjfx/DatabaseController.java
@@ -6,15 +6,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import org.definemodule.businesslogic.controllers.DefineBusinessRuleController;
 import org.domain.Database;
+import org.domain.Table;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseController {
 
-    DefineBusinessRuleController defineController = new DefineBusinessRuleController();
+    private DefineBusinessRuleController defineController = new DefineBusinessRuleController();
+    private TableController tableController = new TableController();
     @FXML
-    ChoiceBox<String> databaseBox;
+    private ChoiceBox<String> databaseBox;
+    private String message;
 
 
 
@@ -36,9 +40,60 @@ public class DatabaseController {
 
     @FXML
     private void nextKnopClick() throws IOException {
-        App.setRoot("table");
-
         String selectedChoice = databaseBox.getSelectionModel().getSelectedItem();
-        System.out.println(selectedChoice);
+        //tableController = new TableController();
+        transferMessageDatabaseToTable(selectedChoice);
+        //App.setRoot("table");
     }
+
+    public void transferMessageDatabaseToTable(String message) throws IOException {
+        String dbNaam = message;
+        List<Database> databases = defineController.giveAllDatabases();
+        for (Database db : databases) {
+            System.out.println("Hallo" + db.getName());
+            if (db.getName().equals(dbNaam)) {
+                System.out.println("geigvdd"+ db.getName());
+                List<Table> allTables = defineController.giveAllTablesByDatabase(db);
+                ObservableList<String> options = FXCollections.observableArrayList();
+                for (Table table : allTables) {
+                    System.out.println(table);
+                    options.add(table.getName());
+//                    System.out.println(getTableController());
+//                    System.out.println(getTableController().tablesBox);
+//                    tableController.tablesBox.getItems().addAll(options);
+                    //tableController.setTables(allTables);
+                    //tableController.initialize(allTables);
+                    try {
+                        tableController.initialize(allTables);
+                        //App.setRoot("table");
+                    }catch (Exception e){
+                        e.toString();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public TableController getTableController(){
+        return tableController;}
+//    public String getMessage(){
+//        return message;
+//    }
+
+//    private void loadSceneAndSendMessage(String messageString, String resourceString) {
+//        try {
+//            //Load second scene
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource(resourceString));
+//            Parent root = loader.load();
+//
+//            //Show scene 2 in new window
+//            Stage stage = new Stage();
+//            stage.setScene(new Scene(root));
+//            stage.setTitle("Second Window");
+//            stage.show();
+//        } catch (IOException ex) {
+//            System.err.println(ex);
+//        }
+//    }
 }
--- src/main/java/org/openjfx/TableController.java
+++ src/main/java/org/openjfx/TableController.java
@@ -1,13 +1,53 @@
 package org.openjfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import org.domain.Table;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TableController {
 
     @FXML
+    public ChoiceBox<String> tablesBox;
+    public List<Table> allTables;
+
+
+    public void fillTables(List<Table> allTables){
+        tablesBox = new ChoiceBox<>();
+        ObservableList<String> options = FXCollections.observableArrayList();
+        for(Table table : allTables){
+            options.add(table.getName());
+            System.out.println("tablecontroller " + table);
+        }
+        System.out.println(tablesBox);
+        tablesBox.getItems().addAll(options);
+    }
+    public void initialize(List<Table> allTables) throws IOException{
+
+        fillTables(allTables);
+        runTable();
+
+    }
+
+    public void setTables(List<Table> tables){
+        allTables = tables;
+    }
+
+    public ChoiceBox<String> getTablesBox() {
+        return tablesBox;
+    }
+
+    @FXML
     private void nextKnopClick() throws IOException {
         App.setRoot("column");
+    }
+
+    @FXML
+    private void runTable() throws IOException {
+        App.setRoot("table");
     }
 }
