6f5df35a2f0c113cdc350b1ac4865846536cde18
lennart-1 <lennartkoster@live.nl>
2020-08-28 18:37:55 +0200

    App class updated

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.definemodule.businesslogic.services.postgresservice.PostgresServiceProvider;
 
 import java.io.IOException;
 
@@ -18,6 +19,14 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        PostgresServiceProvider postgresServiceProvider = new PostgresServiceProvider();
+        TableController tableController = new TableController();
+        String message = "dniudnjldndl";
+        //List<Database> databases = postgresServiceProvider.getDatabasePostgresService().getDatabaseByName(message);
+        //Database database = databases.get(0);
+
+        //System.out.println(database.getName());
+
         scene = new Scene(loadFXML("primary"));
         stage.setScene(scene);
         stage.show();
@@ -28,7 +37,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(org.openjfx.App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
